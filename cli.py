8a9b16a447b93b436560124a1bb016d5526c9a07
Khalil Dias <84887289+KhalilDiasDev@users.noreply.github.com>
2023-07-29 01:22:48 -0300

    Add files via upload

---
+++ cli.py
@@ -0,0 +1,54 @@
+import argparse
+import requests
+
+def consultar_cep(cep):
+    # Verificação se o CEP é uma string vazia ou contém apenas espaços em branco
+    if not cep.strip():
+        print(" =========== CEP vazio ===========")
+        exit()
+
+    endereco = f"https://viacep.com.br/ws/{cep}/json/"
+    resquest = requests.get(endereco)
+
+    # Validação do dado inserido
+    if resquest.status_code == 200:
+        dados_cep = resquest.json()
+        if "erro" in dados_cep:
+            print("=========== CEP não encontrado ===========")
+            exit()
+        return dados_cep
+    elif len(cep) >= 9:
+        print(" =========== CEP inválido ===========")
+        exit()
+    elif resquest.status_code == 400:
+        print(" =========== CEP inválido ===========")
+        exit()
+    else:
+        print("=========== Erro na consulta do CEP =========== ")
+        exit()
+
+def exibir_cep(dados_cep):
+    print("==============================================")
+    print("Resultado da Busca: CEP:", dados_cep["cep"])
+    print("Logradouro:", dados_cep["logradouro"])
+    print("Complemento:", dados_cep["complemento"])
+    print("Bairro:", dados_cep["bairro"])
+    print("Cidade:", dados_cep["localidade"])
+    print("Estado:", dados_cep["uf"])
+    print("==============================================")
+    
+
+def main():
+    parser = argparse.ArgumentParser(description="Utilitário de consulta de CEP")
+    parser.add_argument("-cep", required=True, help="Número do CEP para consulta")
+    args = parser.parse_args()
+
+    try:
+        dados_cep = consultar_cep(args.cep)
+        exibir_cep(dados_cep)
+    except ValueError as e:
+        print("Erro:", e)
+
+if __name__ == "__main__":
+    main()
+
